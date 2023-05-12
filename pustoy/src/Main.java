83277e66c0439a63b2efd8a5343596ad553988cc
Камила <lendramykha@mail.ru>
2023-05-12 17:25:05 +0300

    ss

---
+++ pustoy/src/Main.java
@@ -0,0 +1,13 @@
+package jm.task.core.jdbc;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
